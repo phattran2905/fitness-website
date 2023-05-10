dbeb00094d2252f52008f72c9f8ecec4aaf02e6b
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-09 19:09:18 -0700

    Add SelectedPage type

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "@/scenes/navbar";
+import { SelectedPage } from "./shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>("home");
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
 
   return (
     <div className="app bg-gray-20">
