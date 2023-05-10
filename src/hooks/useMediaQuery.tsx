212f4bac89f34c648bf83ac3a18bb5f48ed88e3f
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-09 19:47:09 -0700

    Add ActionButton

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -1,4 +1,3 @@
-import { match } from "assert";
 import { useEffect, useState } from "react";
 
 const useMediaQuery = (query: string) => {
@@ -13,7 +12,7 @@ const useMediaQuery = (query: string) => {
     // Listen on the resize event
     const listener = () => setMatches(media.matches);
     window.addEventListener("resize", listener);
-    
+
     return () => window.removeEventListener("resize", listener);
   }, [matches, query]);
 
