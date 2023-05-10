0b525f9dada83942df9b75c905611473c8d5abcd
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-09 18:39:46 -0700

    Add Navbar amd Link component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "@/scenes/navbar";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+
   return (
-    <div className='app bg-gray-20'>
-      App
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,24 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {
+  page: string;
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
+
+function Link({ page, selectedPage, setSelectedPage }: Props) {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "");
+
+  return (
+    <AnchorLink
+      href={lowerCasePage}
+      className={`${
+        selectedPage === lowerCasePage
+      } ? "text-primary-500 transition duration-500 hover:text-primary-500`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+}
+export default Link;
